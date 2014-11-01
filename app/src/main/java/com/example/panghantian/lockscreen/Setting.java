8e87ac0bbf9625dfa54ba717462a45f1b1379f05
ppaanngggg <ppaanngggg@gmail.com>
2014-11-01 22:25:25 +0800

    no message

---
+++ app/src/main/java/com/example/panghantian/lockscreen/Setting.java
@@ -0,0 +1,47 @@
+package com.example.panghantian.lockscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+
+public class Setting extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_setting, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        Log.d("pressure : ",Float.toString(event.getPressure()));
+        Log.d("size : ",Float.toString(event.getSize()));
+        return true;
+    }
+}
